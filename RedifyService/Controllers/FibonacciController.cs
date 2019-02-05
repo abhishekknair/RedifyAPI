2db0b51cd0c07308288bcb667399d704a4c860df
FLEETPARTNERS\naira <abhishek.nair@eclipx.com>
2019-02-05 11:52:21 +1100

    no message

--- RedifyService/Controllers/FibonacciController.cs
+++ RedifyService/Controllers/FibonacciController.cs
@@ -23,6 +23,10 @@ namespace RedifyService.Controllers
 
                     return StatusCode(200,"The request is invalid");
                 }
+                if (Double.IsNaN(Convert.ToDouble(req.n)))
+                {
+                    return StatusCode(200, "A value in the fibonacci sequence can only be number");
+                }
                 var l = Convert.ToInt64(req.n);
                 if (l == 0)
                     return Ok(0);
@@ -35,17 +39,17 @@ namespace RedifyService.Controllers
             }
             catch(Exception ex)
             {
-                return StatusCode(500,"Error");
+                return StatusCode(500, "A value in the fibonacci sequence can only be number");
             }
         }
 
         private long GetNthFibonacciNumber(long n)
         {
+
             long number = n - 1; //Need to decrement by 1 since we are starting from 0
             long[] Fib = new long[number + 1];
             Fib[0] = 0;
             Fib[1] = 1;
-            Double d;
 
             for (long i = 2; i <= number; i++)
             {
